4e56d88edb48c5060a6cc70de16f76a469cf76d8
Anderson Gaspar de Medeiros <andersongasparm@gmail.com>
2018-08-07 21:56:57 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import FTPP
 
 print('\n\n===Aplicação para transferência de arquivos ===\n\n')
 
-address = input('Informe o endereço IP: ')
-port = input('Informe a porta: ')
+#address = input('Informe o endereço IP: ')
+address = str('192.168.0.10')
+#port = input('Informe a porta: ')
+port = str(5555)
 
 while(True):
 	print(' Menu: '+'\n')
@@ -13,17 +15,38 @@ while(True):
 
 	if(option == '1'):
 		files = input('Digite o nome do arquivo: \n')
-		recv = FTPP.send(address, port, files, 'testerecv.jpg')
-		if(recv == '0'):
+		recv = FTPP.send(address, int(port), files, 'testerecv.jpg')
+		if(recv == 0):
 			print('Arquivo enviado com sucesso!\n')
+			input('\nPrecione Enter para continuar\n\n')
+		elif(recv == 34):
+			print('Conexão perdida\n')
+			input('\nPrecione Enter para continuar\n\n')
+		elif(recv == 17):
+			print('Arquivo inexistente.\n')
+			input('\nPrecione Enter para continuar\n\n')
+		elif(recv == 18):
+			print('Permissão do arquivo negada.\n')
+			input('\nPrecione Enter para continuar\n\n')
+		elif(recv == 33):
+			print('Número de tentativas excedida.\n')
 			input('\nPrecione Enter para continuar\n\n')
 		else:
 			print(recv)
 			input('\nPrecione Enter para continuar\n\n')
 	elif(option == '2'):
 		recv = FTPP.receive(address, int(port))
-		if(recv == '0'):
+		if(recv == 0):
 			print('Arquivo recebido com sucesso: \n')
+			input('\nPrecione Enter para continuar\n\n')
+		elif(recv == 34):
+			print('Conexão perdida\n')
+			input('\nPrecione Enter para continuar\n\n')
+		elif(recv == 19):
+			print('Espaço insuficiente em disco.\n')
+			input('\nPrecione Enter para continuar\n\n')
+		elif(recv == 33):
+			print('Número de tentativas excedida.\n')
 			input('\nPrecione Enter para continuar\n\n')
 		else:
 			print(recv)
@@ -31,4 +54,4 @@ while(True):
 	else:
 		print('Opção inválida!\n')
 		input('\nPrecione Enter para continuar\n\n')
-	
+
